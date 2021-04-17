5e8202545e92397f4455cde0f845918d571b6bd3
shakil hasan <shshakilhasan69@gmail.com.com>
2021-04-17 02:42:36 -0700

    samthikn change

--- index.js
+++ index.js
@@ -166,6 +166,6 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-  // console.log(`Example app listening at http://localhost:${port}`)
+ 
   console.log('Example app listening on port')
 })
