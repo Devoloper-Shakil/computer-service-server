25179ce41a3751d74c7ca1ccd7718753d57ed54e
shakil hasan <shshakilhasan69@gmail.com.com>
2021-04-17 05:00:53 -0700

    This is a new commit for what I originally planned to be amended

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors());
-const port =  5000;
-
-
+const port =  4000
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xcxqb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -165,4 +163,4 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(process.env.PORT || port)
+app.listen(process.env.port || port)
